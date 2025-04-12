6348627a1f0d69267a2b4fc2d8fe1cff498c477b
fdezbarroso <f.fdezbarroso@gmail.com>
2025-04-13 00:53:21 +0200

    Add scizzors + floating movement script

---
+++ Frogrror/Assets/Scripts/FloatingMovement.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class FloatingMovement : MonoBehaviour
+{
+    public float speed = 0.5f;
+    public float amplitude = 1.0f;
+
+    private Vector2 _startingPosition = Vector2.zero;
+
+    private void Start()
+    {
+        _startingPosition = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        transform.position = _startingPosition + Vector2.up * (amplitude * Mathf.Sin(Time.time * speed));
+    }
+}
