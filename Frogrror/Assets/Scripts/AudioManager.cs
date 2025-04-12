8032f946c83ce83b4a9ab382cb658a9d5af75e0c
aemc64 <aemc.64@gmail.com>
2025-04-12 16:56:20 +0200

    Added audio manager.

--- Frogrror/Assets/Scripts/AudioManager.cs
+++ Frogrror/Assets/Scripts/AudioManager.cs
@@ -2,8 +2,27 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager Instance { get; private set; }
+    
     [SerializeField] private AudioSource _ambienceAudioSource;
     
+    private Camera _camera;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        _camera = Camera.main;
+    }
+
     public void StopAmbience()
     {
         _ambienceAudioSource.Stop();
@@ -12,5 +31,10 @@ public class AudioManager : MonoBehaviour
     public void PlayAmbience()
     {
         _ambienceAudioSource.Play();
+    }
+
+    public void PlaySoundEffect(AudioClip clip)
+    {
+        AudioSource.PlayClipAtPoint(clip, _camera.transform.position);
     }
 }
